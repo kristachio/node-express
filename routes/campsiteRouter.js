6e58b9a3f1a190111808cb6be6eebb342a2b660a
Krista Frietas <krista.frietas@gmail.com>
2023-06-03 11:30:44 -0400

    Workshop 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,24 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('Not supported!');
+})
+.put((req, res) => {
+    res.end(`Will update the campsite: ${req.body.name} and description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite with id: ${req.params.campsiteId}`);
+});
+
 module.exports = campsiteRouter;
